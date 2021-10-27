df38f7bb6e902708e4982648892b33b27f1c8e94
Hodgiecode <55938752+Hodgiecode@users.noreply.github.com>
2021-10-27 13:24:37 +0500

    Add files via upload

---
+++ Триангуляция Делоне/bentley_ottman.cpp
@@ -0,0 +1,6 @@
+#include "bentley_ottman.h"
+
+bentley_ottman::bentley_ottman()
+{
+
+}
---
+++ Триангуляция Делоне/drawwidget.h
@@ -0,0 +1,31 @@
+#ifndef DRAWWIDGET_H
+#define DRAWWIDGET_H
+#include <QWidget>
+#include "task_data.h"
+
+class DrawWidget : public QWidget
+{
+    Q_OBJECT;
+
+public :
+    task_data * pTask;
+
+public:
+    explicit DrawWidget(QWidget *parent = nullptr);
+    void SetDefaultPoints();
+
+    void paintEvent(QPaintEvent *event);
+    void SetTaskPtr(task_data * p) { pTask = p; }
+
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+
+protected:
+    bool m_bMovePointFlag;
+    int last_moved_point_id;
+signals:
+
+};
+
+#endif // DRAWWIDGET_H
